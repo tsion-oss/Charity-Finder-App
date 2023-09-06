01ecc0e9578e82f2d83c200007113cf7fb00526b
Tsion <tsionwendmu12@gmail.com>
2023-09-06 14:43:34 -0400

    s

--- charity-finder/src/App.jsx
+++ charity-finder/src/App.jsx
@@ -4,11 +4,51 @@ import HomePage from './components/HomePage'
 import CharityDetailPage from './components/CharityDetailPage'
 import FavoritePage from './components/FavoritesPage'
 import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
 import { NavLink } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function App() {
  
+  const [charity, setCharity] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+ const [filtereCharities, setFilteredCharities] = useState([]);
+
+
+
+ const fetchCharityData = async () => {
+  try {
+    const response = await axios.get(`https://partners.every.org/v0.2/browse/animals?apiKey=pk_live_b05a8e753c108474a4bc46986c53df22`);
+    setCharity(response.data.nonprofits);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+useEffect(() => {
+  fetchCharityData();
+}, []);
+
+
+ 
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredCharities = charity.filter((char) =>
+    char.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const [charit, getCharit] = useState([])
+
+  useEffect(() => {
+    const getApi = async () => {
+      const response = await axios.get(`https://partners.every.org/v0.2/browse/animals?apiKey=pk_live_b05a8e753c108474a4bc46986c53df22`)
+
+      getCharit(response.data.nonprofits)
+    }
+  })
 
   return (
     <div className='App'> 
@@ -16,12 +56,36 @@ function App() {
     <header>
             <ul>
                 <input type='text' 
-                        placeholder='Search charitieis....'></input>
-                <button>search</button>
+                        placeholder='Search charitieis....'
+                        value={searchQuery}
+                        onChange={handleInputChange}></input>
+                <button onClick={fetchCharityData}>search</button>
                 <li><NavLink to='/'>Charity Finder</NavLink></li>
                 <li><NavLink to='/favoritepage'>Favorite</NavLink></li>
             </ul>
        </header>
+    
+       <div className="charity-list">
+   
+       {searchQuery ? (
+          filteredCharities.length > 0 ? (
+            filteredCharities.map((charity) => (
+              <div key={charity.id}>
+                <Link to={`/charity/${charity.name}`}>
+                   <h2>{charity.name}</h2>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <p></p>
+          )
+        ) : (
+          <p></p>
+        )}
+     
+     </div>
+     
+
   
      <Routes>
        <Route path='/' element={<HomePage/>}/>
--- charity-finder/src/components/Nav.jsx
+++ charity-finder/src/components/Nav.jsx
@@ -4,26 +4,6 @@ import { useState, useEffect } from 'react';
 
 export default function Nav () {
 
-    const [charity, setCharity] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-  
-    useEffect(() => {
-      const getApi = async () => {
-        const response = await axios.get(`https://partners.every.org/v0.2/browse/animals?apiKey=pk_live_b05a8e753c108474a4bc46986c53df22`);
-        setCharity(response.data.nonprofits);
-      };
-      getApi();
-    }, [])
-  
-    // Event handler for input change
-    const handleInputChange = (event) => {
-      setSearchQuery(event.target.value);
-    };
-  
-    // Filter the list of charities based on the search query
-    const filteredCharities = charity.filter((char) =>
-      char.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
     return(
         <header>
             <ul>
